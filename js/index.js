93f8b995ea25d8cf2aacd31d2bc84dce0703144c
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-10-14 07:03:53 -0300

    grid projects styled

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const projects = [
   {
     name: "dev.finance$",
+    image: "./../assets/projects/devfinances.png",
     deployLink: "https://thirsty-khorana-febd08.netlify.app",
     githubLink: "https://github.com/jfmacedo91/dev.finances",
     description: "Projeto desenvolvido como forma de estudo apenas com HTML, CSS e JS",
@@ -8,6 +9,7 @@ const projects = [
   },
   {
     name: "Dropbox UI Clone",
+    image: "./../assets/projects/dropbox.png",
     deployLink: "https://jfmacedo-clone-dropbox.netlify.app/",
     githubLink: "https://github.com/jfmacedo91/ui-clone-dropbox",
     description: "Projeto desenvolvido como forma de estudo com React, Typescript e Styled Components",
@@ -15,6 +17,7 @@ const projects = [
   },
   {
     name: "LikedIn UI Clone",
+    image: "./../assets/projects/linkedin.png",
     deployLink: "https://jfmacedo-clone-linkedin.netlify.app/",
     githubLink: "https://github.com/jfmacedo91/ui-clone-linkedin",
     description: "Projeto desenvolvido como forma de estudo com React, Typescript e Styled Components",
@@ -22,6 +25,7 @@ const projects = [
   },
   {
     name: "ig.news",
+    image: "./../assets/projects/ignews.png",
     deployLink: "https://ignews-murex-seven.vercel.app/",
     githubLink: "https://github.com/jfmacedo91/ignews",
     description: "Projeto desenvolvido com React, Next.js, Prismic CMS e Fauna DB",
@@ -29,6 +33,7 @@ const projects = [
   },
   {
     name: "Aluraquiz",
+    image: "./../assets/projects/aluraquiz.png",
     deployLink: "https://aluraquiz-seven.vercel.app/",
     githubLink: "https://github.com/jfmacedo91/aluraquiz",
     description: "Projeto desenvolvido com React, Next.js e Styled Components",
@@ -36,6 +41,7 @@ const projects = [
   },
   {
     name: "Clocker",
+    image: "./../assets/projects/clocker.png",
     deployLink: "https://clocker-jfmacedo91.vercel.app/",
     githubLink: "https://github.com/jfmacedo91/clocker",
     description: "Projeto desenvolvido com React, Chakra UI e Firebase",
@@ -43,6 +49,7 @@ const projects = [
   },
   {
     name: "move.it",
+    image: "./../assets/projects/moveit.png",
     deployLink: "https://jfmoveit.vercel.app",
     githubLink: "https://github.com/jfmacedo91/moveit",
     description: "Projeto desenvolvido com React, Next.js e Typescript",
@@ -50,34 +57,37 @@ const projects = [
   }
 ]
 
-function innerHTML(array) {
-  const projectsDom = document.querySelector(".projects")
-  projectsDom.innerHTML = ""
-
-  array.map(project => {
-    const html = `
-      <div class="project">
-        <h3>${ project.name }</h3>
-        <a href=${ project.deployLink } target="_blank">depoy</a>
-        <a href=${ project.githubLink } target="_blank">github</a>
-        <span>${ project.description }</span>
-      </div>
-    `
-
-    projectsDom.innerHTML += html
-  })
+const renderProjects = {
+  all() {
+    this.innerHTML(projects)
+  },
+  filtered(param) {
+    const filteredProjects = projects.filter(project => {
+      return project.techs.includes(param)
+    })
+  
+    this.innerHTML(filteredProjects)
+  },
+  innerHTML(array) {
+    const projectsDom = document.querySelector(".projects")
+    projectsDom.innerHTML = ""
+  
+    array.map(project => {
+      const html = `
+        <div class="project">
+          <p>${ project.name }</p>
+          <img src=${ project.image } />
+          <div class="buttons">
+            <a href=${ project.deployLink } target="_blank">depoy</a>
+            <a href=${ project.githubLink } target="_blank">github</a>
+          </div>
+          <span class="tooltip">${ project.description }</span>
+        </div>
+      `
+  
+      projectsDom.innerHTML += html
+    })
+  }
 }
 
-function renderAllProjects() {
-  innerHTML(projects)
-}
-
-function renderFilteredProjects(param) {
-  const filteredProjects = projects.filter(project => {
-    return project.techs.includes(param)
-  })
-
-  innerHTML(filteredProjects)
-}
-
-renderAllProjects()
+renderProjects.all()
